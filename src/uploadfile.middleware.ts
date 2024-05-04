ae86261bef19c9a32a4c3dd785c5207b1aafd6a0
sasa <sysmaaster@gmail.com>
2024-05-04 21:17:25 +0300

    0.0.01

--- src/uploadfile.middleware.ts
+++ src/uploadfile.middleware.ts
@@ -2,10 +2,13 @@ import path from "path";
 import util from "util";
 import multer from "multer";
 import crypto from "crypto";
+import log from "./logger.service";
 
 const maxSize = 25 * 1024 * 1024;
 
-const getCrypto = (): string => {  return crypto.randomBytes(6).toString("hex");};
+const getCrypto = (): string => {
+  return crypto.randomBytes(6).toString("hex");
+};
 const _basedir = path.resolve(path.resolve(), "static");
 
 let storage = multer.diskStorage({
@@ -18,11 +21,24 @@ let storage = multer.diskStorage({
   },
 
   filename: (req: any, file: any, cb: (arg0: null, arg1: any) => void) => {
+    log.info(file);
+    let prefix = "";
+    if (file.mimetype === "image/jpeg") {
+      prefix = "IMG";
+    }
+    if (file.mimetype === "video/mp4") {
+      prefix = "VID";
+    }
+    if (file.mimetype === "video/avi") {
+      prefix = "VID";
+    }
     let ext = file.originalname.substring(
       file.originalname.lastIndexOf("."),
       file.originalname.length
     );
-    cb(null, getCrypto() + ext);
+    let Crypto = getCrypto();
+    log.info(`${file.originalname} => ${prefix}_${Crypto}${ext}`);
+    cb(null, prefix +"_"+ Crypto + ext);
     //cb(null, file.originalname +"_crypto_"+ getCrypto()+ ext);
   },
 });
